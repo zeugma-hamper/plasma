982b0481be036ee036fbd4010d6d0d73fee84ef8
Mignon Pelletier <code@funwithsoftware.org>
2026-09-14 17:28:12 -0700

    Document the map returned by plasma_info().

--- libPlasma/c/plasma-info.h
+++ libPlasma/c/plasma-info.h
@@ -19,6 +19,11 @@ extern "C" {
  * configured, such as whether libYaml and OpenSSL are enabled, and if
  * so, which versions of those libraries are used.  The exact format
  * is subject to change.
+ *
+ * Currently, the map returned contains two keys, "YAML" and "TLS".
+ * If the corresponding feature is enabled, the value is a string
+ * containing the version number of the library.  If the corresponding
+ * feature is disabled, the value is the boolean false.
  */
 OB_PLASMA_API slaw plasma_info (void);
 
